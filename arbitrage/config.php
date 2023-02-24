b4d68503dfceb3ee07e91476f0393f3d47cf8dfd
LogicalMagician <126224887+LogicalMagician@users.noreply.github.com>
2023-02-24 05:01:33 +0000

    Create config.php

---
+++ arbitrage/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// CoinGecko API endpoint
+define('API_ENDPOINT', 'https://api.coingecko.com/api/v3');
+
+// Exchange rate for USD
+define('USD_EXCHANGE_RATE', 1.0); // Change this to match your currency if needed
+
+// Minimum spread percentage for an arbitrage opportunity
+define('MIN_SPREAD_PERCENTAGE', 0.5); // Change this to your desired minimum spread percentage
